b2caa9a4b3c5cd31541f6bbdbf3d8a171275cc74
fbiernat <filipbiernat@gmail.com>
2021-12-04 14:05:38 +0100

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
