97084e6d626045a64ea73c8194e86b3a063baa77
fbiernat <filipbiernat@gmail.com>
2022-01-11 12:04:59 +0100

    Extract application data to separate module

---
+++ src/repository.js
@@ -0,0 +1,47 @@
+export const RamenMenu1 = [
+  {
+    name: "Ramen Tonkotsu Yokohama Style",
+    desc: "Chasu, menma, blanszowany szpinak, jajo nitamago, drobno siekana cebula, olej z palonego czosnku, siekany por, nori i...",
+    price: "36,00 zł",
+    imgURL: "assets/tonkotsu.jpeg",
+  },
+  {
+    name: "Tantanmen",
+    desc: "Mięsny ramen na gęstym wywarze kotteri z dodatkiem mielonej wieprzowiny marynowanej na ostro. Kotteri to ciężki, bogaty wywar...",
+    price: "35,00 zł",
+    imgURL: "assets/tantanmen.jpeg",
+  },
+  {
+    name: "Spicy Chicken",
+    desc: "Ramen na wywarze drobiowym. Długo gotowany, klarowny, głęboki, z dodatkiem ostrego chilli tare. Na toppingu: marynowane na ostro...",
+    price: "37,00 zł",
+    imgURL: "assets/spicy-chicken.jpeg",
+  },
+  {
+    name: "Asakusa Tori Shoyu",
+    desc: "Unikalne shoyu przygotowane wspólnie z mistrzem Kohei Yagi. Drobiowo-wieprzowy bulion assari, specjalnie dedykowany makaron, karkówka...",
+    price: "37,00 zł",
+    imgURL: "assets/asakusa.jpeg",
+  },
+  {
+    name: "Hokkaido",
+    desc: "Wegański, ryżowy wywar z dedykowanym, sezamowym tare na bazie tahiny i miso. Na toppingu: pieczona dynia hokkaido, tofu, por, dymka, nitki...",
+    price: "37,00 zł",
+    imgURL: "assets/hokkaido.jpeg",
+  },
+];
+
+export const RamenMenu2 = [
+  {
+    name: "Miso Ramen",
+    desc: "Ramen na wywarze drobiowym z dodatkiem tare miso - pasty ze sfermentowanych nasion soi. Na toppingu: wołowina, jajko ajitsuke, palony por...",
+    price: "37,00 zł",
+    imgURL: "assets/miso.jpeg",
+  },
+  {
+    name: "Kimchi Ramen",
+    desc: "Autorski ramen na bazie wody spod fermentacji naszego kimchi i klarownego wywaru drobiowego assari. Na toppingu: schab sous vide, jajko ajitsuke, kimchi...",
+    price: "37,00 zł",
+    imgURL: "assets/kimchi.jpeg",
+  },
+];
