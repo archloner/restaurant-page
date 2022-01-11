1eb42c76f0d27e0bf9cf9fa5359c6a915ce8734e
fbiernat <filipbiernat@gmail.com>
2022-01-11 12:05:14 +0100

    Use extracted data

--- src/body-builder.js
+++ src/body-builder.js
@@ -1,56 +1,12 @@
 import { ABOUT_US, MENU, CONTACT } from "./site-builder";
+import { RamenMenu1, RamenMenu2 } from "./repository";
 
 export class BodyBuilder {
-  menu1 = [
-    {
-      name: "Ramen Tonkotsu Yokohama Style",
-      desc: "Chasu, menma, blanszowany szpinak, jajo nitamago, drobno siekana cebula, olej z palonego czosnku, siekany por, nori i...",
-      price: "36,00 zł",
-      imgURL: "assets/tonkotsu.jpeg",
-    },
-    {
-      name: "Tantanmen",
-      desc: "Mięsny ramen na gęstym wywarze kotteri z dodatkiem mielonej wieprzowiny marynowanej na ostro. Kotteri to ciężki, bogaty wywar...",
-      price: "35,00 zł",
-      imgURL: "assets/tantanmen.jpeg",
-    },
-    {
-      name: "Spicy Chicken",
-      desc: "Ramen na wywarze drobiowym. Długo gotowany, klarowny, głęboki, z dodatkiem ostrego chilli tare. Na toppingu: marynowane na ostro...",
-      price: "37,00 zł",
-      imgURL: "assets/spicy-chicken.jpeg",
-    },
-    {
-      name: "Asakusa Tori Shoyu",
-      desc: "Unikalne shoyu przygotowane wspólnie z mistrzem Kohei Yagi. Drobiowo-wieprzowy bulion assari, specjalnie dedykowany makaron, karkówka...",
-      price: "37,00 zł",
-      imgURL: "assets/asakusa.jpeg",
-    },
-    {
-      name: "Hokkaido",
-      desc: "Wegański, ryżowy wywar z dedykowanym, sezamowym tare na bazie tahiny i miso. Na toppingu: pieczona dynia hokkaido, tofu, por, dymka, nitki...",
-      price: "37,00 zł",
-      imgURL: "assets/hokkaido.jpeg",
-    },
-  ];
-
-  menu2 = [
-    {
-      name: "Miso Ramen",
-      desc: "Ramen na wywarze drobiowym z dodatkiem tare miso - pasty ze sfermentowanych nasion soi. Na toppingu: wołowina, jajko ajitsuke, palony por...",
-      price: "37,00 zł",
-      imgURL: "assets/miso.jpeg",
-    },
-    {
-      name: "Kimchi Ramen",
-      desc: "Autorski ramen na bazie wody spod fermentacji naszego kimchi i klarownego wywaru drobiowego assari. Na toppingu: schab sous vide, jajko ajitsuke, kimchi...",
-      price: "37,00 zł",
-      imgURL: "assets/kimchi.jpeg",
-    },
-  ];
+  menu1 = RamenMenu1;
+  menu2 = RamenMenu2;
 
   test() {
-    console.log("BodyBuilder works");
+    console.log("BodyBuilder works out");
   }
 
   build(contentId) {
